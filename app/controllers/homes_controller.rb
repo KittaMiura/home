2346821b1cc7002c520e707184991c0c7f04973d
KittaMiura <112059987+roppou@users.noreply.github.com>
2022-12-04 08:34:00 +0000

    [Add]CSS

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,21 @@
+class HomesController < ApplicationController
+
+  def top
+  end
+
+  def about
+  end
+
+  def bio
+  end
+
+  def works
+  end
+  
+  def links
+  end
+  
+  def contact
+  end
+
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,6 +74,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.hosts << "01d037e4bf534f32a52f92c1a03ab2e9.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  config.hosts.clear
 
 end
